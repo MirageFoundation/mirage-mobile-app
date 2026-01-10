406b2815991983f47f719bbb404e53d99ead5b66
Sonali <sonalirajput091@gmail.com>
2026-01-10 13:28:29 +0530

    app config change

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ import { ConfigContext, ExpoConfig } from "expo/config";
 
 const env = process.env.EXPO_PUBLIC_ENV || "";
 const bundleIdentifier = env
-  ? `talk.mirage.app.${env}`
-  : `talk.mirage.app`;
+  ? `talk.mirage.mobile.${env}`
+  : `talk.mirage.mobile`;
 const scheme = env ? `shadow${env}` : `mirage`;
 
 const name = env ? `mirage (${env.toUpperCase()})` : "mirage";
