fe87a75d89f5c2e2afa5cd043dc875c096419280
nitishxyz <nitishxyz@proton.me>
2026-01-16 12:43:42 +0530

    feat: remove webassembly

--- app.config.ts
+++ app.config.ts
@@ -92,7 +92,6 @@ export default ({ config }: ConfigContext): ExpoConfig => {
           photosPermission: "$(PRODUCT_NAME) needs access to your Photos.",
         },
       ],
-      "react-native-webassembly",
     ],
     experiments: {
       typedRoutes: true,
