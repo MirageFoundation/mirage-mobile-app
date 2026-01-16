78d2ade872c9b0a0ca950efb43328237a12f67d7
nitishxyz <nitishxyz@proton.me>
2026-01-16 22:57:47 +0530

    add new argon2 lib

--- packages/react-native-argon2-turbo/ios/Argon2Turbo-Bridging-Header.h
+++
@@ -1,6 +0,0 @@
-#ifndef Argon2Turbo_Bridging_Header_h
-#define Argon2Turbo_Bridging_Header_h
-
-#import <React/RCTBridgeModule.h>
-
-#endif
--- packages/react-native-argon2-turbo/ios/Argon2Turbo.h
+++
@@ -1,5 +0,0 @@
-#import <Argon2TurboSpec/Argon2TurboSpec.h>
-
-@interface Argon2Turbo : NSObject <NativeArgon2TurboSpec>
-
-@end
