0200c0809f0fbbb39cab2d5739c3e22e68ce3abb
Sonali <sonalirajput091@gmail.com>
2026-05-16 00:17:26 +0530

    chore(ios): bump version to 1.0.14

--- app.config.ts
+++ app.config.ts
@@ -101,7 +101,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name,
     slug,
-    version: "1.0.13",
+    version: "1.0.14",
     orientation: "default",
     icon: "./assets/images/icon.png",
     scheme: scheme,
