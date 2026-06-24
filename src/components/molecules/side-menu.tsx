05961a76f61074d4258299ae2fa37f8cd0c24a9f
Sonali <sonalirajput091@gmail.com>
2026-06-25 04:14:31 +0530

    chore(side-menu): bump version references from 207 to 208
    - Update version text from 207 to 208 in first menu item
    - Update version text from 207 to 208 in second menu item

--- src/components/molecules/side-menu.tsx
+++ src/components/molecules/side-menu.tsx
@@ -799,7 +799,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 207
+                          update 208
                         </Text>
                         <Text
                           style={{
@@ -851,7 +851,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 207
+                          update 208
                         </Text>
                         <Text
                           style={{
