ec5ea26b23d4875d52c155c521fef8315e749d23
Sonali <sonalirajput091@gmail.com>
2026-06-25 03:21:03 +0530

    chore(side-menu): bump update version from 205 to 206
    - Update first version label from 205 to 206
    - Update second version label from 205 to 206

--- src/components/molecules/side-menu.tsx
+++ src/components/molecules/side-menu.tsx
@@ -799,7 +799,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 205
+                          update 206
                         </Text>
                         <Text
                           style={{
@@ -851,7 +851,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 205
+                          update 206
                         </Text>
                         <Text
                           style={{
