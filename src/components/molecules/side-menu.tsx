416a89b3a4e7357b9878fd4c91c7784e28fef10d
Sonali <sonalirajput091@gmail.com>
2026-07-17 05:13:17 +0530

    chore(side-menu): update version strings from 214 to 215
    - Update first version string occurrence from 214 to 215
    - Update second version string occurrence from 214 to 215
    - Bump version references in side menu text labels

--- src/components/molecules/side-menu.tsx
+++ src/components/molecules/side-menu.tsx
@@ -851,7 +851,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 214
+                          update 215
                         </Text>
                         <Text
                           style={{
@@ -928,7 +928,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 214
+                          update 215
                         </Text>
                         <Text
                           style={{
