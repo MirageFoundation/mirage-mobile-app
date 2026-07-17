1aa38093a87d3d00f69427edb1b69ce391f2fab2
Sonali <sonalirajput091@gmail.com>
2026-07-17 22:39:50 +0530

    chore(release): bump app and package version to 1.1.2
    - Update Expo app config version to 1.1.2
    - Update package.json version to 1.1.2

--- app.config.ts
+++ app.config.ts
@@ -102,7 +102,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     name,
     slug,
-    version: "1.1.1",
+    version: "1.1.2",
     orientation: "default",
     icon: "./assets/images/icon.png",
     scheme: scheme,
