869df97b0201dee134e5accde136e7ad4027cba3
Sonali <sonalirajput091@gmail.com>
2026-07-18 18:00:10 +0530

    docs(side-menu): update version references from 216 to 217
    - Update first version reference in side menu text
    - Update second version reference in side menu text
    - Increment documentation version number across component

--- src/components/molecules/side-menu.tsx
+++ src/components/molecules/side-menu.tsx
@@ -851,7 +851,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 216
+                          update 217
                         </Text>
                         <Text
                           style={{
@@ -928,7 +928,7 @@ export const SideMenu = forwardRef<SideMenuRef, SideMenuProps>(
                           size="sm"
                           weight="light"
                         >
-                          update 216
+                          update 217
                         </Text>
                         <Text
                           style={{
